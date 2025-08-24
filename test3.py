9ca36d1265317a6548ee2832ae2ecc8899286b2d
Ali Mustermann <ali@example.com>
2025-08-24 20:32:43 +0200

    Änderungen an test3.py

--- test3.py
+++ test3.py
@@ -1,5 +1,3 @@
-def begruessung(name="Gast"):
-    print("Hallo", name)
-
-begruessung("Ali")
-begruessung()  # benutzt den Standardwert
+import math
+radius = float(input('Bitte Radius eingeben'))
+print("Der Umfang ist ", 2*math.pi *radius)
